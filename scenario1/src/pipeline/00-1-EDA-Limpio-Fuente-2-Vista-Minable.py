eee47c4325338205469a22b97269460c20a639a8
Pedro Nel <pedro_ortiza@javeriana.edu.co>
2024-08-03 15:12:52 -0500

    Actualiza :running:
    Prueba de actualizar cambios

--- scenario1/src/pipeline/00-1-EDA-Limpio-Fuente-2-Vista-Minable.py
+++ scenario1/src/pipeline/00-1-EDA-Limpio-Fuente-2-Vista-Minable.py
@@ -18,7 +18,7 @@ from pandas import DataFrame, merge
 # sys.path.insert(1, ROOT_PATH)
 
 #FIXME: Persona demo -> Validar que se esta importando archivo root.py
-#import root
+import root
 
 # Tambien, se puede hacer de esta forma.
 #from root import DIR_DATA
@@ -31,6 +31,7 @@ print(root.DIR_DATA_RAW)
 print("@@@ Imprimiendo ruta de DATA_STAGE")
 #FIXME: Persona demo -> Hacer print de DIR_DAT_STAGE
 
+print(root.DIR_DATA_STAGE)
 
 
 #TODO Importacion inicial de archivo crudo desde data/stage/xx para luego generar pickle de vista minable
@@ -76,44 +77,45 @@ df  = df.rename(columns={
 # Para asignar el nombre de la entidad matriz a cada registro
 indice_nit = pd.read_csv(root.DIR_DATA_RAW + "indice_nit.csv", encoding= 'unicode_escape')
 
-#Renombrar variables
-indice_nit = indice_nit.rename(columns={'NIT': "nit",
-                                        'Entidad': "entidad_matriz"})
+# #Renombrar variables
+# indice_nit = indice_nit.rename(columns={'NIT': "nit",
+#                                         'Entidad': "entidad_matriz"})
 
-# Se genera merge
-df = indice_nit.merge(df, on='nit', how='left')
-df
+# # Se genera merge
+# df = indice_nit.merge(df, on='nit', how='left')
+# df
 
-# Eliminar filas de 'modalidad' que no le interesan a Caoba.
-df=df[df["modalidad"].str.contains("03|04|05|07|10|11|15|17|18")==False]
-print(df.modalidad.unique())
+# # Eliminar filas de 'modalidad' que no le interesan a Caoba.
+# df=df[df["modalidad"].str.contains("03|04|05|07|10|11|15|17|18")==False]
+# print(df.modalidad.unique())
 
-# Reemplazar el 'mes_proyectado'==No Definido por 'Enero'
-df['mes_proyectado'].replace('No Definido','Enero', inplace=True)
-df['mes_proyectado'].value_counts()
+# # Reemplazar el 'mes_proyectado'==No Definido por 'Enero'
+# df['mes_proyectado'].replace('No Definido','Enero', inplace=True)
+# df['mes_proyectado'].value_counts()
 
-#Realizando casting a variables
-df[['date_last_publication','date_publised']]=df[['date_last_publication','date_publised']].astype('datetime64[ns]')
-df['date_last_publication'].head()
+# #Realizando casting a variables
+# df[['date_last_publication','date_publised']]=df[['date_last_publication','date_publised']].astype('datetime64[ns]')
+# df['date_last_publication'].head()
 
-# Ajustar el formato de 'date_publised' a año-mes-dia
-df['date_publised']=df['date_publised'].dt.strftime('%d/%m/%Y')
-df['date_publised']=pd.to_datetime(df['date_publised'],format='%d/%m/%Y')
+# # Ajustar el formato de 'date_publised' a año-mes-dia
+# df['date_publised']=df['date_publised'].dt.strftime('%d/%m/%Y')
+# df['date_publised']=pd.to_datetime(df['date_publised'],format='%d/%m/%Y')
 
-# **Consideremos los días entre los 2 campos 'date_last_publication' y 'date_publised':**
-df['diff_dates']=(df['date_last_publication']-df['date_publised']).dt.days
-#print(sorted(df.diff_dates.unique()))
+# # **Consideremos los días entre los 2 campos 'date_last_publication' y 'date_publised':**
+# df['diff_dates']=(df['date_last_publication']-df['date_publised']).dt.days
+# #print(sorted(df.diff_dates.unique()))
 
-# Realizando cambios sobre variable year_publised
-df['year_publised'] = pd.DatetimeIndex(df['date_publised']).year
+# # Realizando cambios sobre variable year_publised
+# df['year_publised'] = pd.DatetimeIndex(df['date_publised']).year
 
-df.dropna(inplace=True)
+# df.dropna(inplace=True)
 
 
 #TODO Exportando archivo serializado de Fuente_2_vista_minable.pickle. Debe usar extension .pickle o .pkl
 
 #FIXME: Persona demo -> relacionar
-output_fuente2 = <> + "Fuente_2_vista_minable.pickle"
+#output_fuente2 = <> + "Fuente_2_vista_minable.pickle"
+output_fuente2 = df_secopII.pickle + "Fuente_2_vista_minable.pickle"
 #print(output_fuente2)
 print(f'%%% Generando archivo serializado en data/stage/ de vista minable del dataframe => df')
 print(f'%%% Archivo se encuentra en {output_fuente2}')
